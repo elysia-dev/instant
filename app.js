b6d1e8898a1d14645a1199a8581fedc4b8c87df1
donguk.seo <donguk.seo@modori.io>
2020-11-30 15:01:49 +0900

    Remove legacy files

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var http = require("http");
-var app = express();
-var server = http.createServer(app);
-app.use(express.static('public'))
-
-server.listen(3000, "127.0.0.1", function () {
-  console.log("Server listen on port " + server.address().port);
-});
--- deploy.sh
+++
@@ -1,2 +0,0 @@
-aws s3 sync ./dist s3://elysia.land
-aws cloudfront create-invalidation --distribution-id E3CKXTXNRTCQ0A --paths "/*"
