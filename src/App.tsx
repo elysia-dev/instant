1163e9c6aeb307166b7100ef9a0b80d6f52ce79b
donguk.seo <donguk.seo@modori.io>
2020-12-14 10:51:36 +0900

    Init user language

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // import './css/style.scss';
 import './mobile/css/mobileStyle.scss';
 import {
@@ -16,7 +16,7 @@ import Footer from './modules/footer/Footer';
 import RouteWithHeader from './modules/RouteWithHeader';
 import ScrollToTop from './modules/ScrollToTop';
 
- /*mobile */
+/*mobile */
 import MainMobile from './mobile/modules/main/Main';
 import UserAgreementMobile from './mobile/modules/UserAgreement';
 import PrivacyPolicyMobile from './mobile/modules/PrivacyPolicy';
@@ -25,63 +25,77 @@ import FooterMobile from './mobile/modules/footer/Footer';
 
 import MediaQuery from "react-responsive";
 import { useMediaQuery } from "react-responsive";
+import { useTranslation } from 'react-i18next';
 
 
 const App = () => {
+  const { i18n } = useTranslation();
   const isPc = useMediaQuery({
     query: "(min-width: 768px)"
   });
   const isMobile = useMediaQuery({
     query: "(max-width: 767px)"
   });
+
+  useEffect(() => {
+    // ref http://www.lingoes.net/en/translator/langcode.htm
+    const lang = navigator.language;
+
+    if (lang.includes('ko')) {
+      i18n.changeLanguage('ko')
+    } else if (lang.includes('zh')) {
+      i18n.changeLanguage('zh')
+    }
+  }, [])
+
   const PcRouter = () => {
     return (
-    <Router>
-      <ScrollToTop />
-      <Switch>
-        <RouteWithHeader path="/contact">
-          <Contact />
-        </RouteWithHeader>
-        <RouteWithHeader path="/privacyPolicy">
-          <PrivacyPolicy />
-        </RouteWithHeader>
-        <RouteWithHeader path="/disclaimer">
-          <Disclaimer />
-        </RouteWithHeader>
-        <Route path="*">
-          <Main />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+      <Router>
+        <ScrollToTop />
+        <Switch>
+          <RouteWithHeader path="/contact">
+            <Contact />
+          </RouteWithHeader>
+          <RouteWithHeader path="/privacyPolicy">
+            <PrivacyPolicy />
+          </RouteWithHeader>
+          <RouteWithHeader path="/disclaimer">
+            <Disclaimer />
+          </RouteWithHeader>
+          <Route path="*">
+            <Main />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     );
   }
   const MobileRouter = () => {
     return (
-    <Router>
-      <Switch>
-        <Route path="/userAgreement">
-          <UserAgreementMobile />
-        </Route>
-        <Route path="/privacyPolicy">
-          <PrivacyPolicyMobile />
-        </Route>
-        <Route path="/disclaimer">
-          <DisclaimerMobile />
-        </Route>
-        <Route path="*">
-          <MainMobile />
-        </Route>
-      </Switch>
-      <FooterMobile />
-    </Router>
+      <Router>
+        <Switch>
+          <Route path="/userAgreement">
+            <UserAgreementMobile />
+          </Route>
+          <Route path="/privacyPolicy">
+            <PrivacyPolicyMobile />
+          </Route>
+          <Route path="/disclaimer">
+            <DisclaimerMobile />
+          </Route>
+          <Route path="*">
+            <MainMobile />
+          </Route>
+        </Switch>
+        <FooterMobile />
+      </Router>
     );
   }
   return (
-    isPc ? 
-    <PcRouter/> 
-    :
-    <MobileRouter />
+    isPc ?
+      <PcRouter />
+      :
+      <MobileRouter />
   );
 }
 
