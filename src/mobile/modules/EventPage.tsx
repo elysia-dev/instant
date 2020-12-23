f3fbdb5638b64f8678068118112522d85f59b158
hyunoh-modori <hyunoh.bae@modori.io>
2020-12-24 01:48:06 +0900

    change EventPage

--- src/mobile/modules/EventPage.tsx
+++ src/mobile/modules/EventPage.tsx
@@ -1,104 +1,114 @@
 import React from 'react';
 import '../css/mobileStyle.scss';
 import ElysiaLogo from '../../shared/image/Elysia_Logo.png';
+import ImageEvent_01 from '../../shared/image/event/image_mobile_event_01.png';
 import EventSlider from './slider/EventSlider';
 import { Link, useHistory } from 'react-router-dom';
 
 const EventPage = () => {
-  
+  var root = window.document.getElementById("root");
+  root!.style.background = "linear-gradient(rgb(132, 13, 6) 95px, rgb(132, 13, 6) 567px, rgb(81, 18, 13) 509px, rgb(81, 18, 13) 677px, rgb(255, 255, 255) 670px)";
+
   const history = useHistory();
   return (
-    <div className="wrapper__event--mobile">
-      <header className="mobile-header-container">
-        <Link to="/">
-          <img src={ElysiaLogo} className="elysia-logo" alt="Elysia" />
-        </Link>
-      </header>
-      <div className="eventpage-container__moblie" style={{ width: 300, margin: '8vh auto' }}>
-        <h1 className="text--header__moblie bold">엘리시아 앱 상품 출시<br />기념 이벤트</h1>
-        <p className="text--header__label__moblie">
-          엘리시아 모바일 전용 상품 출시를 기념하여<br />엘리시아 고객들을 대상으로<br />‘엘리시아 에셋 1호’ 증정 이벤트를 실시합니다.
-        </p>
-        <div className="container__building-slider__mobile">
-          <EventSlider />
-        </div>
-        <h1 className="Text__header__mobile bold">지급방식</h1>
-        <div className="Text-wrapper__mobile">
-          <p className="Text__insert__mobile">경품 증여</p>
-        </div>
-        <h1 className="Text__header__mobile bold">부동산 정보</h1>
-        <div className="Text-wrapper__mobile">
-          <h1 className="Text__insert__mobile bold">주소</h1>
-          <p className="Text__insert__mobile" style={{ fontSize: 12 }}>서울특별시 강남구 논현동 149-18번지 1개호실 <br/>
-          <span>(* 구체적인 호수는 개인정보보호차원에서 당첨자에게만 공개됩니다.)</span></p>
-          <div className="Wrapper__text-wrapper">
-            <div style={{ flex: 1 }}>
-              <h1 className="Text__insert__mobile bold">시세</h1>
-              <p className="Text__insert__mobile">2억원</p>
+    <div className="eventpage-container__mobile" style={{ width: 300, margin: '0px auto', backgroundColor: "#FFF", paddingBottom: 90 }}>
+      <img src={ImageEvent_01} className="image__header__mobile" alt="Elysia"/>
+      <div className="container__image--bottom">
+      * 본 이벤트는 엘리시아 재단과<br />소속 법인인 ELYSIA PLATFORM PTE. LTD에서<br />기획하고 진행하였으며 책임 소재는 재단에게 있습니다.
+      </div>
+      <div className="container__slider-wrapper__mobile">
+        <EventSlider />
+      </div>
+      <h1 className="text__table-header">부동산 정보</h1>
+      <table className="table--event-page__mobile" style={{ marginBottom: 10 }}>
+        <tr>
+          <td><h1 className="text__td--first__mobile">주소</h1></td>
+          <td>
+            <span  style={{ fontSize: 14 }}>서울특별시 강남구 논현동 149-18번지 1개호실 </span>
+            <br /><p className="text--footer__mobile">* 구체적인 호수는 개인정보보호차원에서 당첨자에게만 공개됩니다.</p>
+          </td>
+        </tr>
+        <tr>
+          <td><h1 className="text__td--first__mobile">시세</h1></td>
+          <td>
+            <p style={{ fontSize: 14 }}>2억원</p>
+          </td>
+        </tr>
+        <tr>
+          <td><h1 className="text__td--first__mobile">임대<br />보증금</h1></td>
+          <td>
+            <p style={{ fontSize: 14 }}>1.6억원</p>
+          </td>
+        </tr>
+        <tr>
+          <td><h1 className="text__td--first__mobile">세입자<br />계약기간</h1></td>
+          <td>
+            <p style={{ fontSize: 14 }}>~2022년 7월</p>
+          </td>
+        </tr>
+        <tr>
+          <td><h1 className="text__td--first__mobile">세금관련<br />안내</h1></td>
+          <td>
+            <div>
+              <span style={{ fontSize: 14 }}>다음과 같은 세금이 발생합니다.</span>
+              <br /><span className="line-height__mobile" style={{ fontSize: 14 }}>- 취득세 : 약 160만원<br />- 지방교육세 : 약 16만원</span>
             </div>
-            <hr style = {{ marginRight: 26, height: 50 }} />
-            <div style={{ flex: 1 }}>
-              <h1 className="Text__insert__mobile bold">전세보증금</h1>
-              <p className="Text__insert__mobile">1.6억원</p>
-            </div>
-          </div>
-          <h1 className="Text__insert__mobile bold">세입자 계약기간</h1>
-          <p className="Text__insert__mobile">~ 2022년 7월</p>
-        </div>
-        <h1 className="Text__header__mobile bold">증빙서류</h1>
-        <div className="Text-wrapper__mobile">
-          <h1 className="Text__insert__mobile bold">
-            부동산등기부등본    
-          </h1>
-          <a href="https://drive.google.com/file/d/16McnIM8GQ2ugqo-5vVkJjZalVm9A72F9/view?usp=sharing">
-            <p className="Text__hyperlink__mobile">
-              보러가기
+          </td>
+        </tr>
+      </table>
+      <p className="Text__label--footer__mobile">
+        * 등기과정에서의 법무사비용 및 고지된 세목 이외의 세금발생시 엘리시아 재단에서 부담합니다.
+      </p>
+
+
+      <h1 className="text__table-header">증빙서류</h1>
+      <table className="table--event-page__mobile">
+        <tr>
+          <td style={{ width: 75 }}>
+            <h1 className="text__td--first__mobile">부동산<br />등기부등본</h1>
+          </td>
+          <td>
+            <a href="https://drive.google.com/file/d/1g4xqw8eBbKC24QgXyIW_rkqaugeCh7Nk/view?usp=sharing" target='_blank' className="Text__link">
+              <p className="button__hyperlink__mobile">바로가기</p>
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td style={{ width: 75 }}>
+            <h1 className="text__td--first__mobile">전세계약서</h1>
+          </td>
+          <td>
+            <a href="https://drive.google.com/file/d/1g4xqw8eBbKC24QgXyIW_rkqaugeCh7Nk/view?usp=sharing" target='_blank' className="Text__link">
+              <p className="button__hyperlink__mobile">바로가기</p>
+            </a>
+          </td>
+        </tr>
+      </table>
+
+      <h1 className="text__table-header">FAQ</h1>
+      <table className="table--event-page__mobile" style={{ marginBottom: 80 }}>
+        <tr>
+          <td>
+            <p className="QNA--label__mobile">
+              <span className="QNA__bold">Q)</span> 임차인 관리는 어떻게 하나요? <br /><span className="QNA__bold">A)</span> 파트너 부동산을 통해 잔여 임대기간동안 관리해드립니다.
             </p>
-          </a>
-          <h1 className="Text__insert__mobile bold">
-            전세계약서  
-          </h1>
-          <a href="https://drive.google.com/file/d/1g4xqw8eBbKC24QgXyIW_rkqaugeCh7Nk/view?usp=sharing">
-            <p className="Text__hyperlink__mobile">
-              보러가기
-            </p>
-          </a>
-        </div>
-        <h1 className="Text__header__mobile bold">경품수령시 세금안내</h1>
-        <p>다음과 같은 세금이 당첨자분께 발생됩니다.</p>
-        <div className="Text-wrapper__mobile" style={{ margin: 0 }}>
-          <div className="Wrapper__text-wrapper" style={{margin: 0}}>
-            <div style={{ flex: 1 }}>
-              <h1 className="Text__insert__mobile bold">취득세</h1>
-            <p className="Text__insert__mobile">약 160만원</p>
-            </div>
-            <hr style = {{ marginRight: 26, height: 50 }} />
-            <div style={{ flex: 1 }}>
-              <h1 className="Text__insert__mobile bold">지방교육세</h1>
-              <p className="Text__insert__mobile">약 16만원</p>
-            </div>
-          </div>
-        </div>
-        <p className="Text__label--footer__mobile">* 기타 비용(등기 등)은 엘리시아 재단에서 부담합니다.</p>
-        <br /><br />
-        <h1 className="Text__header__mobile bold">FAQ</h1>
-        <p className="QNA--label__mobile">
-          <span className="QNA__bold">Q)</span> 임차인 관리는 어떻게 하나요? <br /><span className="QNA__bold">A)</span> 파트너 부동산을 통해 잔여 임대기간동안 관리해드립니다.
-        </p>
-        <hr />
-        <p className="QNA--label__mobile">
+          </td>
+        </tr>
+        <tr>
+          <td ><p className="QNA--label__mobile">
           <span className="QNA__bold">Q)</span> 직접 살고 싶은데 방법이 있을까요? <br /><span className="QNA__bold">A)</span> 전세계약 만료일인 2022년 7월 4일 이후 입주가능합니다.
-        </p>
-        <hr />
-        <p className="QNA--label__mobile">
+        </p></td>
+        </tr>
+        <tr>
+          <td><p className="QNA--label__mobile">
           <span className="QNA__bold">Q)</span> 바로 매각하고 싶은데 어떻게 하면 되나요? <br /><span className="QNA__bold">A)</span> 파트너 부동산을 통해 매각절차진행을 도와드립니다.
-        </p>
-        <hr />
-        <br /><br />
-        <h1 className="text--header__moblie bold">기타 궁금하신 문의사항은 아래로 문의바랍니다.</h1>
-        <div style={{ textAlign: "center"}}>
-          <button className="mobile-contact-button" onClick={() => history.push("/contact")}>문의하기</button>
-        </div>
+        </p></td>
+        </tr>
+      </table>
+
+      <h1 className="text--header__moblie">기타 궁금하신 사항은<br />아래로 문의바랍니다</h1>
+      <div style={{ textAlign: "center"}}>
+        <button className="mobile-contact-button" onClick={() => history.push("/contact")}>문의하기</button>
       </div>
     </div>
   );
--- src/modules/EventPage.tsx
+++ src/modules/EventPage.tsx
@@ -6,92 +6,117 @@ import BuildingImg03 from '../shared/image/event/BuildingImg03.png';
 import BuildingImg04 from '../shared/image/event/BuildingImg04.png';
 import BuildingImg05 from '../shared/image/event/BuildingImg05.png';
 import BuildingImg06 from '../shared/image/event/BuildingImg06.png';
+import ImageEvent_01 from '../shared/image/event/image_event_01.png';
 import { Link, useHistory } from 'react-router-dom';
+import EventSlider from './slider/EventSlider';
 
 const EventPage = () => {
-  
+  var root = window.document.getElementById("root");
+  root!.style.background = "linear-gradient(rgb(255, 255, 255) 95px,rgb(132, 13, 6) 95px, rgb(132, 13, 6) 778px, rgb(81, 18, 13) 780px, rgb(81, 18, 13) 892px, rgb(255, 255, 255) 894px)";
+
   const history = useHistory();
   return (
-    <div className="eventpage-container" style={{ maxWidth: 1190, margin: '15vh auto', padding: 30 }}>
-      <h1 className="text--header bold">엘리시아 앱 상품 출시 기념 이벤트</h1>
-      <p className="text--header__label">
-        엘리시아 모바일 전용 상품 출시를 기념하여 엘리시아 고객들을 대상으로 <span style={{ color: "#3679B5"}}>‘엘리시아 에셋 1호’</span> 증정 이벤트를 실시합니다.<br />
-      </p>
-      <div className="container__building-image">
-        <img src={BuildingImg01} className="img--building" alt="Elysia"/>
-        <img src={BuildingImg02} className="img--building img--building__right" alt="Elysia"/>
-        <img src={BuildingImg03} className="img--building" alt="Elysia"/>
-        <img src={BuildingImg04} className="img--building img--building__right" alt="Elysia"/>
-        <img src={BuildingImg05} className="img--building" alt="Elysia"/>
-        <img src={BuildingImg06} className="img--building img--building__right" alt="Elysia"/>
+    <div className="eventpage-container" style={{ maxWidth: 1160, margin: '20px auto', backgroundColor: "#FFF", paddingBottom: 90 }}>
+      <img src={ImageEvent_01} className="image__header" alt="Elysia" />
+      <div className="container__slider-wrapper">
+        <EventSlider />
       </div>
-      
-      <div className="Dot--Text" /><h1 className="Text__Dot-header">지급방식</h1>
-      <div className="Text-wrapper">
-        <p className="Text__insert">경품 증여</p>
-      </div>
-      <div className="Dot--Text" /><h1 className="Text__Dot-header">부동산 정보</h1>
-      <div className="Text-wrapper">
-        <p className="Text__insert">
-          (1) 주소 : 서울특별시 강남구 논현동 149-18번지 1개호실 
-          <br /><p className="Text__insert-label">* 구체적인 호수는 개인정보보호차원에서 당첨자에게만 공개됩니다.</p>
-        </p>
-        <p className="Text__insert">
-          (2) 시세 : 2억원
-        </p>
-        <p className="Text__insert">
-          (3) 전세보증금 : 1.6억원
-        </p>
-        <p className="Text__insert">
-          (4) 세입자 계약기간 : ~ 2022년 7월    
-        </p>
-      </div>
-      <div className="Dot--Text" /><h1 className="Text__Dot-header">증빙서류</h1>
-      <div className="Text-wrapper">
-        <p className="Text__insert">
-          (1) 부동산등기부등본    
-        </p>
-        <a href="https://drive.google.com/file/d/16McnIM8GQ2ugqo-5vVkJjZalVm9A72F9/view?usp=sharing">
-          <p className="Text__hyperlink">
-              https://drive.google.com/file/d/16McnIM8GQ2ugqo-5vVkJjZalVm9A72F9/view?usp=sharing
-          </p>
-        </a>
-        <p className="Text__insert">
-          (2) 전세계약서  
-        </p>
-        <a href="https://drive.google.com/file/d/1g4xqw8eBbKC24QgXyIW_rkqaugeCh7Nk/view?usp=sharing">
-          <p className="Text__hyperlink">
-            https://drive.google.com/file/d/1g4xqw8eBbKC24QgXyIW_rkqaugeCh7Nk/view?usp=sharing
-          </p>
-        </a>
-        
-      </div>
-      <div className="Dot--Text" /><h1 className="Text__Dot-header">경품수령시 세금안내</h1>
-      <p className="Text__label">다음과 같은 세금이 당첨자분께 발생됩니다.</p>
-      <div className="Text-wrapper Text-wrapper--footer">
-        <p className="Text__insert">(1) 취득세 : 약 160만원</p>
-        <p className="Text__insert">(2) 지방교육세 : 약 16만원</p>
-      </div>
+      <table className="table--event-page" style={{ marginBottom: 10 }}>
+        <tr>
+          <td><h1 className="text__td--first">지급 방식</h1></td>
+          <td colSpan={2}>부담부 증여</td>
+        </tr>
+        <tr>
+          <td rowSpan={3}><h1 className="text__td--first">부동산 정보</h1></td>
+          <td colSpan={2}>
+            <div>
+              <span className="bold" style={{ fontSize: 25 }}>주소</span>
+              <br /><span  style={{ fontSize: 24 }}>서울특별시 강남구 논현동 149-18번지 1개호실 </span>
+              <br /><p className="text--footer">* 구체적인 호수는 개인정보보호차원에서 당첨자에게만 공개됩니다.</p>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div>
+              <span className="bold" style={{ fontSize: 25 }}>시세</span>
+              <br /><span  style={{ fontSize: 24 }}>2억원</span>
+            </div>
+          </td>
+          <td>
+            <div>
+              <span className="bold" style={{ fontSize: 25 }}>임대보증금</span>
+              <br /><span  style={{ fontSize: 24 }}>1.6억원</span>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td colSpan={2}>
+            <div>
+              <span className="bold" style={{ fontSize: 25 }}>세입자 계약기간</span>
+              <br /><span  style={{ fontSize: 24 }}>~2022년 7월</span>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td><h1 className="text__td--first">세금관련 안내</h1></td>
+          <td colSpan={2}>
+            <div>
+              <span style={{ fontSize: 24 }}>다음과 같은 세금이 당첨자분께 발생됩니다.</span>
+              <br /><span className="line-height" style={{ fontSize: 24 }}>- 취득세 : 약 160만원<br />- 지방교육세 : 약 16만원</span>
+            </div>
+          </td>
+        </tr>
+      </table>
       <p className="Text__label--footer">
-      * 기타 비용(등기 등)은 엘리시아 재단에서 부담합니다.
+        * 등기과정에서의 법무사비용 및 고지된 세목 이외의 세금발생시 엘리시아 재단에서 부담합니다.
       </p>
-      <div className="Dot--Text" /><h1 className="Text__Dot-header">FAQ</h1>
-        <p className="QNA--label">
-          <span className="QNA__bold">Q)</span> 임차인 관리는 어떻게 하나요? <br /><span className="QNA__bold">A)</span> 파트너 부동산을 통해 잔여 임대기간동안 관리해드립니다.
-        </p>
-        <hr style={{ marginLeft: 35 }} />
-        <p className="QNA--label">
+
+      <table className="table--event-page">
+        <tr>
+          <td><h1 className="text__td--first">증빙서류</h1></td>
+          <td>
+          <span className="bold" style={{ fontSize: 25 }}>부동산 등기부등본</span>
+            <br />
+            <a href="https://drive.google.com/file/d/16McnIM8GQ2ugqo-5vVkJjZalVm9A72F9/view?usp=sharing" target='_blank' className="Text__link">
+              <p className="button__hyperlink">바로가기</p>
+            </a>
+          </td>
+          <td>
+          <span className="bold" style={{ fontSize: 25 }}>전세계약서</span>
+            <br />
+            <a href="https://drive.google.com/file/d/1g4xqw8eBbKC24QgXyIW_rkqaugeCh7Nk/view?usp=sharing" target='_blank' className="Text__link">
+              <p className="button__hyperlink">바로가기</p>
+            </a>
+          </td>
+        </tr>
+      </table>
+
+      <table className="table--event-page" style={{ marginBottom: 80 }}>
+        <tr>
+          <td rowSpan={3}><h1 className="text__td--first">FAQ</h1></td>
+          <td colSpan={2}>
+            <p className="QNA--label">
+              <span className="QNA__bold">Q)</span> 임차인 관리는 어떻게 하나요? <br /><span className="QNA__bold">A)</span> 파트너 부동산을 통해 잔여 임대기간동안 관리해드립니다.
+            </p>
+        </td>
+        </tr>
+        <tr>
+          <td colSpan={2}><p className="QNA--label">
           <span className="QNA__bold">Q)</span> 직접 살고 싶은데 방법이 있을까요? <br /><span className="QNA__bold">A)</span> 전세계약 만료일인 2022년 7월 4일 이후 입주가능합니다.
-        </p>
-        <hr style={{ marginLeft: 35 }} />
-        <p className="QNA--label">
+        </p></td>
+        </tr>
+        <tr>
+          <td colSpan={2}><p className="QNA--label">
           <span className="QNA__bold">Q)</span> 바로 매각하고 싶은데 어떻게 하면 되나요? <br /><span className="QNA__bold">A)</span> 파트너 부동산을 통해 매각절차진행을 도와드립니다.
-        </p>
-        <hr style={{ marginLeft: 35 }} />
-        <h1 className="text--header__contact">기타 궁금하신 문의사항은 아래로 문의바랍니다.</h1>
-        <div style={{ textAlign: "center"}}>
-          <button className="mobile-contact-button" onClick={() => history.push("/contact")}>문의하기</button>
-        </div>
+        </p></td>
+        </tr>
+      </table>
+
+      <h1 className="text--header__contact">기타 궁금하신 사항은 아래로 문의바랍니다</h1>
+      <div style={{ textAlign: "center"}}>
+        <button className="button__contact" onClick={() => history.push("/contact")}>문의하기</button>
+      </div>
     </div>
   );
 }
