7ff5c1d5f0df41d7d849992a1eade53d8b46b685
hyunoh-modori <hyunoh.bae@modori.io>
2022-02-22 18:14:42 +0900

    add mediaquery

--- src/pages/AppNavigator.tsx
+++ src/pages/AppNavigator.tsx
@@ -4,31 +4,42 @@ import {
   Route,
   BrowserRouter as Router,
 } from "react-router-dom";
+
+import '../stylesheets/public.scss';
+import '../stylesheets/pc.scss';
+import '../stylesheets/mobile.scss';
+
 import Main from 'src/components/Main';
 import Footer from 'src/components/Footer';
 import ScrollToTop from 'src/pages/utiles/ScrollToTop';
+import useMediaQueryType from "src/hooks/useMediaQueryType";
+import MediaQuery from "src/enums/MediaQuery";
+
 
 const AppNavigator: React.FC = () => {
+  const { value: mediaQuery } = useMediaQueryType();
 
   return (
-    <Router>
-      <ScrollToTop />
-      <Switch>
-        {/* <RouteWithHeader path="/privacyPolicy">
-          <PrivacyPolicy />
-        </RouteWithHeader>
-        <RouteWithHeader path="/disclaimer">
-          <Disclaimer />
-        </RouteWithHeader>
-        <Route path="/AppLink">
-          <AppPage />
-        </Route> */}
-        <Route path="*">
-          <Main />
-        </Route>
-        <Footer />
-      </Switch>
-    </Router>
+    <div className={`elysia ${mediaQuery === MediaQuery.PC ? 'view-w' : 'view-m'}`}>
+      <Router>
+        <ScrollToTop />
+        <Switch>
+          {/* <RouteWithHeader path="/privacyPolicy">
+            <PrivacyPolicy />
+          </RouteWithHeader>
+          <RouteWithHeader path="/disclaimer">
+            <Disclaimer />
+          </RouteWithHeader>
+          <Route path="/AppLink">
+            <AppPage />
+          </Route> */}
+          <Route path="*">
+            <Main />
+          </Route>
+          <Footer />
+        </Switch>
+      </Router>
+    </div>
   )
 }
 
